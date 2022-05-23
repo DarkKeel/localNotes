933d1d38689e0083b1f2919251b4c4fb7e1ebda9
Boris Makarov <boris_makarov@epam.com>
2022-05-23 09:57:17 +0300

    LN30: reafactoring id generation util

--- backend-application/src/main/java/com/localnotes/utils/IdGeneration.java
+++ backend-application/src/main/java/com/localnotes/utils/IdGeneration.java
@@ -1,26 +1,25 @@
 package com.localnotes.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IdGeneration {
 
-    private static final Random random = new Random();
-    private static final int leftLimit = 48;
-    private static final int rightLimit = 122;
-    private static final int targetStringLength = 10;
-
-    private IdGeneration() {
-
-    }
+    private static final Random RANDOM = new Random();
+    private static final int LEFT_LIMIT = 48;
+    private static final int RIGHT_LIMIT = 122;
+    private static final int TARGET_STRING_LENGTH = 10;
 
     public static String createId() {
 
-        return random.ints(leftLimit, rightLimit + 1)
+        return RANDOM.ints(LEFT_LIMIT, RIGHT_LIMIT + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
+                .limit(TARGET_STRING_LENGTH)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
     }
